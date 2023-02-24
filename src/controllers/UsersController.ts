1812bafc7308bbe3a2b8504c62785c1baae7f26c
Santadd <hethere97@gmail.com>
2023-02-24 15:00:01 +0000

    add pagination functionality

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -2,22 +2,21 @@ import { NextFunction, Request, Response } from "express";
 import { appDataSource } from "../database/data-source";
 import { User } from "../database/entities/UserEntity";
 import { ResponseUtil } from "../utils/Response";
+import { Paginator } from "../database/Paginator";
 
 export class UsersController {
     // Get users function
     async getUsers(req: Request, res: Response, next: NextFunction) {
-        const users = await appDataSource.getRepository(User).find();
+        const builder = appDataSource.getRepository(User).createQueryBuilder().orderBy("id", "DESC");
 
-        console.log(users, "I find them here")
-        return res.status(200).json({
-            success: true,
-            message: "Fetched users successfully",
-            data: users
-        });
+        const {records: users, paginationInfo} = await Paginator.paginate(builder, req)
+        return ResponseUtil.sendResponse(res, "Fetched users successfully", users, paginationInfo)
+        
     }
 
     // Get a user function
     async getUser(req: Request, res: Response, next: NextFunction) {
+
         // get email from the request
         const {id} = req.params;
         const user = await appDataSource.getRepository(User).findOneByOrFail({
